3350bd912110c91a25290097e50b13ca5d44f9db
Alex Shaburov <frosty174@yandex.ru>
2023-04-17 20:10:26 +0500

    added discription on line 55(task 21)

--- Program.cs
+++ Program.cs
@@ -52,7 +52,7 @@
 // Console.Write("Введите координаты Z2: ");
 // int Z2 = Convert.ToInt32(Console.ReadLine());
 // double d = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2) + Math.Pow((Z2 - Z1), 2));
-// Console.WriteLine(Math.Round(d, 2));
+// Console.WriteLine ($"Расстояние между точками: {Math.Round(d, 2)}");
 
 //=======================================================
 
